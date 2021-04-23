fbc6b5631263468b60458561e42620c12c822712
Harun Doğdu <Harundogdu06@gmail.com>
2021-04-23 03:31:39 +0300

    locale added

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const message = document.getElementById("success-message");
 
 const correctLetters = [];
 const wrongLetters = [];
-
 const selectedWord = getRandomWord();
 const playAgain = document.getElementById("btnPlayAgain");
 
@@ -17,8 +16,8 @@ let sayac = 0;
 
 
 function getRandomWord() {
-    const words = ["Adana", "Adıyaman", "Afyon", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"];
-    return words[Math.floor(Math.random() * words.length)].toLowerCase();
+    const words = ["İstanbul"];
+    return words[Math.floor(Math.random() * words.length)].toLocaleLowerCase('tr-TR');
 }
 
 function displayWord() {
